dc01a385e15edabbba7b4c68a611afabbdcadb72
Vladislav Kondratuk <vladislavkondratuk322@gmail.com>
2020-07-11 20:26:47 +0300

    spring-boot project init

---
+++ src/main/java/com/city/info/bot/CityInfoTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.city.info.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInfoTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityInfoTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/city/info/bot/CityInfoTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.info.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityInfoTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
